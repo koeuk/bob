781e1ea88e02f3973561392c776aad9d6d49738d
koeuk kos <koeukkos@gmail.com>
2026-05-11 22:00:33 +0700

    feat: add HasFactory trait to core models and expand database seeder volume

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -43,10 +43,10 @@ class DatabaseSeeder extends Seeder
             ],
         );
 
-        if (app()->environment('local', 'testing') && User::count() < 20) {
-            $users = User::factory(15)->create();
+        if (app()->environment('local', 'testing') && User::count() < 30) {
+            $users = User::factory(25)->create();
 
-            Post::factory(40)
+            $posts = Post::factory(60)
                 ->recycle($users)
                 ->create()
                 ->each(function (Post $post) use ($users) {
@@ -56,11 +56,23 @@ class DatabaseSeeder extends Seeder
                         ->create();
                 });
 
-            Report::factory(8)
+            // Ensure user 23 has at least 10 posts with comments
+            $user23 = User::find(23);
+            if ($user23) {
+                $user23Posts = Post::factory(10)->create(['user_id' => $user23->id]);
+                $user23Posts->each(function (Post $post) use ($users) {
+                    Comment::factory(random_int(3, 5))->recycle($users)->for($post)->create();
+                });
+                Comment::factory(10)->recycle($user23Posts)->create(['user_id' => $user23->id]);
+                Report::factory(10)->recycle($user23Posts)->create(['reporter_id' => $user23->id]);
+                Ban::factory(3)->create(['user_id' => $user23->id, 'banned_by' => $admin->id]);
+            }
+
+            Report::factory(10)
                 ->recycle($users->push($admin, $superAdmin))
                 ->create();
 
-            Ban::factory(3)->recycle($users)->create([
+            Ban::factory(5)->recycle($users)->create([
                 'banned_by' => $admin->id,
             ]);
 
